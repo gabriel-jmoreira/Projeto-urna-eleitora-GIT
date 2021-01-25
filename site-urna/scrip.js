ee5e687cfb41c36f22de5087df874c02c7646a8f
Gabriel Moreira <g.quadrado@outlook.com>
2021-01-25 13:21:01 -0300

    Dandos os primeiros passos no proejto para JS
    Começo do script

---
+++ site-urna/scrip.js
@@ -0,0 +1,19 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+
+function clicou(n) {
+    alert("Clicou em "+n);
+}
+function branco(n) {
+    alert("Clicou em BRANCO!");
+}
+function corrige() {
+    alert("Clicou em CORRIGE");
+}
+
+function confirma() {
+    alert("Clicou em CONFIRMA");
+}
