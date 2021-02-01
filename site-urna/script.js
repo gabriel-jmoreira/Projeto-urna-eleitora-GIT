db29033af5ba9dc29624c04441d36719b0063a99
Gabriel Moreira <g.quadrado@outlook.com>
2021-02-01 15:57:16 -0300

    Atualização de tela e teclado
    Foi feita agr comandos back-end para funcionar os teclados em conjunto com a tela

---
+++ site-urna/script.js
@@ -0,0 +1,54 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+let etapaAtual = 0;
+let numero = '';
+
+function começarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+
+    for(let i=0;i<etapa.numeros;i++){
+        if(i ===0) {
+            numero += '<div class="numero pisca"></div>';
+        }else {
+        numeroHtml += '<div class="numero "></div>';
+        }
+    }
+
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+function atualizaInterface() {
+
+}
+
+function clicou(n) {
+    let elNumero = document.querySelector('.numero.pisca');
+    if(elNumero !==null){
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+    }
+}
+function branco() {
+    alert("Clicou em Branco!");
+}
+function corrige() {
+    alert("Clicou em Corrige!");
+}
+function confirma() {
+    alert("Clicou em Confirma!");
+}
+
+começarEtapa();
+
