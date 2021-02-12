5727ee877219733dea6aa2c4395db8f23d887ab9
Gabriel Moreira <g.quadrado@outlook.com>
2021-02-12 15:28:38 -0300

    atualização 12

--- site-urna/script.js
+++ site-urna/script.js
@@ -39,25 +39,20 @@ function atualizaInterface() {
             return false;
         }
     });    
-    if(candidato.length > 0){
+    if(candidato.length > 0) {
         candidato = candidato[0];
         seuVotoPara.style.display = 'block';
         aviso.style.display = 'block';
-        descricao.innerHTML = `Nome ${candidato.nome} <br/>Partido: ${candidato.partido}`;
-        
+        descricao.innerHTML = `Nome ${candidato.nome} <br/>Partido: ${candidato.partido}`; 
         let fotosHtml = '';
-        for(let i in candidato.fotos){
-            fotosHtml += `<div class="d-1-image"><img src="image/${candidato.fotos[i].url}"alt="">${candidato.fotos[i].legenda}</div>`;
+        for(let i in candidato.fotos) {
+            fotosHtml +=  `<div class="d-1-image"><img src="image/${candidato.fotos[i].url}"alt="">${candidato.fotos[i].lengenda}</div>`;
         }
+        
         lateral.innerHTML = fotosHtml;
-    }   else {
-        seuVotoPara.style.display = 'block';
-        aviso.style.display = 'block';
-        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>';
+
     }
-}   
-
-
+}
 function clicou(n) {
     let elNumero = document.querySelector('.numero.pisca');
     if(elNumero !== null) {
